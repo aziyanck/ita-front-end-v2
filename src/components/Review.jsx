637f79144fd69313f689b11aadca07a9f2621531
aziyanck <aziyan916@gmail.com>
2025-06-18 13:08:28 +0530

    loading animation done

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,6 +1,8 @@
 
 import { useState, useEffect } from 'react';
 
+import Loader from "../components/animations/Loader"
+
 import TestimonialCard from '../components/sub_comp/TestimonialCard';
 import testimonialData from '../components/sub_comp/testimonial_data';
 import getCombinedReviews from '../components/sub_comp/combined_review';
@@ -37,14 +39,17 @@ function Review() {
 
             <div className="review-div w-screen h-auto flex flex-col justify-around  md:flex-row items-center gap-8 ">
                 <div className="left ">
-                    <h1 data-aos="slide-right" className="text-3xl pl-8 ml-4 md:text-5xl poppins-extrabold text-st p-4">Our Customer Review Says it All</h1>
+                    <h1 data-aos="slide-right" className="text-3xl pl-8 ml-4 md:text-5xl poppins-extrabold gd-text p-4">Our Customer Review Says it All</h1>
 
                 </div>
                 <div className="right w-auto h-full">
 
                     <div data-aos="slide-up" className="p-6 md:p-10 flex flex-wrap gap-6 justify-center">
                         {reviewData.length === 0 ? (
-                            <p className="text-gray-400 text-center">Loading reviews...</p>
+                            <>
+
+                                <Loader />
+                            </>
                         ) : (
                             reviewData.map((review, index) => (
                                 <TestimonialCard
